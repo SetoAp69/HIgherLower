fcc97e68b881ffa0ae24a8a6e7fc158e2a1845b3
Seto <mocatastic77@gmail.com>
2025-01-08 17:29:42 +0700

    Success Adding Google signin authentication using firebase and one tap client

--- app/src/main/java/com/excal/higherlower/MainActivity.kt
+++ app/src/main/java/com/excal/higherlower/MainActivity.kt
@@ -1,20 +1,57 @@
 package com.excal.higherlower
 
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.excal.higherlower.databinding.ActivityMainBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        firebaseAuth=Firebase.auth
+        val currentUser=firebaseAuth.currentUser
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+
+        if(currentUser!=null){
+
+            binding.tvEmail.text=currentUser.email.toString()
+        }else{
+            Log.i(TAG,"Auth Not Found")
+        }
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnSignOut.setOnClickListener{
+            signOut()
+            val intent= Intent(this@MainActivity,SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+
+
+
+    }
+
+    private fun signOut(){
+        firebaseAuth.signOut()
     }
 }
--- app/src/main/java/com/excal/higherlower/SigninActivity.kt
+++ app/src/main/java/com/excal/higherlower/SigninActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.IntentSender
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,26 +15,27 @@ import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.common.api.ApiException
-import com.google.android.libraries.identity.googleid.GetSignInWithGoogleOption
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.auth
 
 class SigninActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySigninBinding
+    private lateinit var binding:ActivitySigninBinding
+    private lateinit var firebaseAuth:FirebaseAuth
     private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest:BeginSignInRequest
 
-    //Firebase auth
-
-    private val REQ_ONE_TAP = 1
-    private var clientId="874497066463-svjon23sk78l9a25k5o3q7c21vj6lmaq.apps.googleusercontent.com"
-    private lateinit var auth:FirebaseAuth
-
-
+    private val REQ_ONE_TAP=1
+    private var showOneTapUI=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        binding=ActivitySigninBinding.inflate(layoutInflater)
-        auth= Firebase.auth
+
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        firebaseAuth= Firebase.auth
+
+        val signInButton=binding.btnSignIn
+
 
 
         super.onCreate(savedInstanceState)
@@ -44,73 +46,92 @@ class SigninActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.btnSignIn.setOnClickListener{
+
+        signInButton.setOnClickListener{
             signIn()
         }
 
+
     }
 
-    public override fun onStart() {
-        super.onStart()
-        val currentUser=auth.currentUser
-        if(currentUser!=null){
+    private fun signIn(){
 
-        }
-    }
-
-    fun signIn(){
-        oneTapClient = Identity.getSignInClient(this)
-        val signInRequest=BeginSignInRequest.builder()
+        oneTapClient=Identity.getSignInClient(this@SigninActivity)
+        signInRequest=BeginSignInRequest.builder()
             .setGoogleIdTokenRequestOptions(
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                     .setSupported(true)
                     .setServerClientId(getString(R.string.default_web_client_id))
-                    .setFilterByAuthorizedAccounts(true)
                     .build()
             )
             .build()
 
 
         oneTapClient.beginSignIn(signInRequest)
-            .addOnSuccessListener { result->
-                try{
-
+            .addOnSuccessListener(this) { result->
+                try {
                     startIntentSenderForResult(
-                        result.pendingIntent.intentSender,
-                        REQ_ONE_TAP,
-                        null,
-                        0,
-                        0,
-                        0,
-                        null
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null,0,0,0,null
                     )
                 }catch(e: IntentSender.SendIntentException){
-                    Log.e(TAG,"Error Launching One Tap Sign-in: ${e.localizedMessage}")
+                    Log.e(TAG,"Couldn't start One Tap UI : ${e.localizedMessage}")
                 }
-            }
-            .addOnFailureListener{e->
-                Log.e(TAG,"Error Launching One Tap Sign-in: ${e.localizedMessage}")
 
             }
+            .addOnFailureListener(this){e->
+                Log.d(TAG,e.localizedMessage)
+            }
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser=firebaseAuth.currentUser
+        if(currentUser!=null){
+            val intent=Intent(this@SigninActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(requestCode==REQ_ONE_TAP){
-            try{
-                val credential=oneTapClient.getSignInCredentialFromIntent(data)
-                val idToken=credential.googleIdToken
-                val username=credential.displayName
-                val email=credential.id
-                if (idToken != null) {
-                    Log.d("OneTap", "Got ID token: $idToken")
-                    // Send the token to your backend
-                } else {
-                    Log.d("OneTap", "No ID token or credentials.")
+        when(requestCode){
+            REQ_ONE_TAP->{
+                try{
+                    val credential=oneTapClient.getSignInCredentialFromIntent(data)
+                    val idToken=credential.googleIdToken
+                    when{
+                        idToken!=null->{
+                            val firebaseCredential=GoogleAuthProvider.getCredential(idToken,null)
+                            firebaseAuth.signInWithCredential(firebaseCredential)
+                                .addOnCompleteListener(this){
+                                    task->
+                                    if(task.isSuccessful){
+                                        val user=firebaseAuth.currentUser
+                                        val intent=Intent(this@SigninActivity,MainActivity::class.java)
+                                        startActivity(intent)
+                                        finish()
+                                    }else{
+                                        Toast.makeText(this@SigninActivity,"Failed to signin using credential",Toast.LENGTH_LONG)
+
+                                    }
+                                }
+
+                        }
+                        else->{
+                            Toast.makeText(this@SigninActivity,"Id Token Not Found",Toast.LENGTH_LONG)
+                        }
+                    }
+
+                }catch (e:ApiException){
+                    Log.w(TAG,"Error Sign in with One Tap Client : ${e.localizedMessage}")
                 }
-            } catch (e: ApiException) {
-                Log.e("OneTap", "Sign-in failed: ${e.localizedMessage}")
             }
         }
     }
+
+
 }
